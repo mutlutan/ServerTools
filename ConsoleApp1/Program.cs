338f1386e1d704035515aab83b7100df5a8875ba
mutlutan <mutlutan@outlook.com>
2025-05-04 04:39:27 +0300

    fix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,12 +8,14 @@ using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Management;
 
 namespace ConsoleApp1
 {
 	class Program
 	{
 		static int timeOutDakika = 10;
+
 		static void Main(/*string[] args*/)
 		{
 			Task.Factory.StartNew(() =>
@@ -21,10 +23,13 @@ namespace ConsoleApp1
 				Job1();
 			});
 
-			Console.WriteLine($"{timeOutDakika} dakika internet kesilir ise sunucu yeniden başlatılır.");
+            Console.WriteLine($"{timeOutDakika} dakika internet kesilir ise sunucu yeniden başlatılır.");
 			Console.ReadLine();
 
-		}
+			
+
+
+        }
 
 		static void LogSave(string text)
 		{
@@ -49,7 +54,7 @@ namespace ConsoleApp1
 				string pingTarget = "8.8.8.8"; // Google DNS, bağlantıyı kontrol etmek için
 				using (Ping ping = new Ping())
 				{
-					var reply = ping.Send(pingTarget, 1000); // 1 saniye timeout
+					var reply = ping.Send(pingTarget, 2000); // 2 saniye timeout
 					return reply.Status == IPStatus.Success;
 				}
 			}
@@ -73,7 +78,40 @@ namespace ConsoleApp1
 			}
 		}
 
-		static void Job1()
+        static void RestartNetworkAdapter()
+        {
+            try
+            {
+                string adapterName = "Ethernet"; // Ağ adaptörünün adı
+                string query = $"SELECT * FROM Win32_NetworkAdapter WHERE NetConnectionID = '{adapterName}'";
+
+                string message0 = $"Restart Network Adapter ({adapterName}) Başlatıldı: {DateTime.Now}";
+                LogSave(message0);
+                Console.WriteLine(message0);
+
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+                foreach (ManagementObject adapter in searcher.Get())
+                {
+                    adapter.InvokeMethod("Disable", null);
+                    string message1 = $"Network Adapter ({adapterName}) Devre dışı bırakıldı...: {DateTime.Now}";
+                    LogSave(message1);
+                    Console.WriteLine(message1);
+
+                    Thread.Sleep(3000);
+
+                    adapter.InvokeMethod("Enable", null);
+                    string message2 = $"Network Adapter ({adapterName}) Etkinleştirildi. {DateTime.Now}";
+                    LogSave(message2);
+                    Console.WriteLine(message2);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: " + ex.Message);
+            }
+        }
+
+        static void Job1()
 		{
 			int timeout = 1000 * 60 * timeOutDakika; // timeOutDakika:10 dakika (10 dk = 600000 milisaniye)
 			DateTime connectionLostTime = DateTime.Now;
@@ -92,7 +130,6 @@ namespace ConsoleApp1
 				}
 				else
 				{
-
 					var gecenSure = (DateTime.Now - connectionLostTime);
 					// Eğer timeout dakika geçtiyse, sunucuyu yeniden başlat
 					if (gecenSure.TotalMilliseconds >= timeout)
@@ -108,8 +145,12 @@ namespace ConsoleApp1
 						string message = $"Internet bağlantısı kayboldu: {DateTime.Now}";
 						LogSave(message);
 						Console.WriteLine(message);
-					}
-				}
+
+						// network ü yeniden başlat
+						RestartNetworkAdapter();
+                        Thread.Sleep(1000 * 60 * 1);
+                    }
+                }
 
 
 				// 1 dk bekle ve tekrar kontrol et
